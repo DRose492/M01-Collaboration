6c158926a052216132beb1f44538dc42351b00cd
DRose492 <146631485+DRose492@users.noreply.github.com>
2023-11-06 17:32:30 -0600

    GPA Checker.py
    Misread a portion of the assignment and came to fix it.

--- GPA Checker.py
+++ GPA Checker.py
@@ -3,20 +3,23 @@
 # Checks to see if a given student's name and GPA qualifies for specified marks/ranks
 
 # Detailing the variables needed
-name = ""
+first_name = ""
+last_name = ""
 grade = 0
 
 # Writing the code
-name = input("What is the name of the student? ")
+first_name = input("What is the first name of the student? ")
+last_name = input("What is the last name of the student? ")
 grade = float(input("What is this student's GPA? "))
 
-while name != "ZZZ":
+while last_name != "ZZZ":
     if grade > 3.49:
-        print(name + "'s GPA would make them the Dean's List!!!")
+        print(first_name + last_name + "'s GPA would make them the Dean's List!!!")
     elif grade > 3.24 and grade < 3.5:
-        print(name + "'s GPA has made the Honor Roll")
+        print(first_name + last_name + "'s GPA has made the Honor Roll")
     else:
-        print(name + "'s GPA didn't make either the Honor Roll or the Dean's List")
-    print()    
-    name = input("Please enter the next student. If there is no more students to enter, enter ZZZ ")
+        print(first_name + last_name + "'s GPA didn't make either the Honor Roll or the Dean's List")
+    print()
+    first_name = input("Please the next student's first name. ")   
+    last_name = input("Please the next student's last name. If there is no more students to enter, enter ZZZ ")
     grade = float(input("Enter the next student's GPA "))
